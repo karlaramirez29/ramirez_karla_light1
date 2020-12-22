e0635633178960bf5edbc87190df436413c9a681
karlaramirez29 <72756396+karlaramirez29@users.noreply.github.com>
2020-12-22 09:35:35 -0500

    Update pxt.json, main.ts, main.py

--- main.ts
+++ main.ts
@@ -1,1 +1,11 @@
-
+while (true) {
+    console.log("light_level:" + input.lightLevel())
+    if (input.lightLevel() > 20) {
+        light.setAll(light.rgb(0, 0, 0))
+    } else if (input.lightLevel() >= 10) {
+        light.setAll(light.rgb(0, 0, 255))
+    } else {
+        light.setAll(light.rgb(255, 144, 3))
+    }
+    
+}
